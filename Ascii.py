b650ffb5e6b111ea7a5dd89320c03254f03798ef
ko009380 <108348363+SeanKitaka@users.noreply.github.com>
2023-04-13 16:17:00 +0100

    setting up files and starting

---
+++ Ascii.py
@@ -0,0 +1,22 @@
+"""
+import PIL
+from PIL import Image
+import requests
+from io import BytesIO
+from PIL import ImageFilter
+from PIL import ImageEnhance
+from IPython.display import display
+import numpy as np
+"""
+DENSITY = "@%#*+=-:. "
+
+def colourToAscii(image_path, width=100):
+    # Open the image and convert it to grayscale
+    # Resize the image based on the width parameter
+    # Convert the pixels to ASCII characters
+    # Return the ASCII string
+    
+    pass
+def greyToAscii(image_path,width=100):
+    
+    pass
